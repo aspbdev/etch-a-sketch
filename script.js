582a04f0543edaf1774e767c82178ef38c9fbc1e
Aspas <aspasdev@gmail.com>
2021-10-31 16:09:45 +0300

    Add draw and clear functionality

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ for (let i = 0; i < 256; i++) {
     square.className = 'square';
     container.append(square);
 }
+
+const squareItems = document.querySelectorAll('.square');
+
+console.log(squareItems);
+squareItems.forEach(item =>
+    item.addEventListener('mouseover', () => {
+        item.classList.add('draw');
+    }));
+
+const clearBtn = document.querySelector('.clear-all');
+
+clearBtn.addEventListener('click', () => {
+    squareItems.forEach(item => item.className = 'square');
+})
