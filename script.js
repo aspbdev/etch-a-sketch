e606d1b0b6076a30a1b0b48034efa30c74560868
Aspas <aspasdev@gmail.com>
2021-11-01 17:42:44 +0300

    Add grid resize functionality

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 const container = document.querySelector('.container');
-
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.className = 'square';
-    container.append(square);
-}
-
-const squareItems = document.querySelectorAll('.square');
-
-console.log(squareItems);
-squareItems.forEach(item =>
-    item.addEventListener('mouseover', () => {
-        item.classList.add('draw');
-    }));
-
+const gridBtn = document.querySelector('#grid-btn');
+const gridInput = document.querySelector('#grid-size');
 const clearBtn = document.querySelector('.clear-all');
 
-clearBtn.addEventListener('click', () => {
-    squareItems.forEach(item => item.className = 'square');
+let gridSize = 16;
+makeGrid(gridSize);
+
+gridBtn.addEventListener('click', () => {
+    const input = +gridInput.value;
+    if (input && input <= 60) {
+        makeGrid(input);
+    }
 })
+
+function makeGrid(size) {
+    container.innerHTML = '';
+    container.setAttribute('style', 
+        `grid-template-columns: repeat(${size}, auto);
+        grid-template-rows: repeat(${size}, auto);`);
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        container.append(square);
+    }
+    
+    const squareItems = document.querySelectorAll('.square');
+    squareItems.forEach(item =>
+        item.addEventListener('mouseover', () => {
+            item.classList.add('draw');
+    }));
+
+    clearBtn.addEventListener('click', () => {
+        squareItems.forEach(item => item.className = 'square');
+    })
+
+    gridInput.value = '';
+}
+
+
+
+
